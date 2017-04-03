ad401b2d32989bb382a12c03901dd2bcd3149ece
Kai Chen <kaichen@outlook.com>
2017-04-03 00:33:24 -0700

    Computing and plotting the learning curve

--- bias-variance/test1.m
+++ bias-variance/test1.m
@@ -60,3 +60,27 @@ hold off;
 
 fprintf('Press enter to continue.\n');
 pause;
+
+
+%% =========== Learning Curve for Linear Regression =============
+
+lambda = 0;
+[error_train, error_val] = ...
+    learningCurve([ones(m, 1) X], y, ...
+                  [ones(size(Xval, 1), 1) Xval], yval, ...
+                  lambda);
+
+plot(1:m, error_train, 1:m, error_val);
+title('Learning curve for linear regression')
+legend('Train', 'Cross Validation')
+xlabel('Number of training examples')
+ylabel('Error')
+axis([0 13 0 150])
+
+fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+for i = 1:m
+    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+end
+
+fprintf('Press enter to continue.\n');
+pause;

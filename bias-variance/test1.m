5b6a25d93035ff4e8b35c4359c3df22899383e9d
Kai Chen <kaichen@outlook.com>
2017-04-04 00:19:56 -0700

    Selecting lambda

--- bias-variance/test1.m
+++ bias-variance/test1.m
@@ -146,3 +146,23 @@ end
 
 fprintf('Press enter to continue.\n');
 pause;
+
+%% =========== Validation for Selecting Lambda =============
+
+[lambda_vec, error_train, error_val] = ...
+    validationCurve(X_poly, y, X_poly_val, yval);
+
+close all;
+plot(lambda_vec, error_train, lambda_vec, error_val);
+legend('Train', 'Cross Validation');
+xlabel('lambda');
+ylabel('Error');
+
+fprintf('lambda\t\tTrain Error\tValidation Error\n');
+for i = 1:length(lambda_vec)
+    fprintf(' %f\t%f\t%f\n', ...
+            lambda_vec(i), error_train(i), error_val(i));
+end
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;

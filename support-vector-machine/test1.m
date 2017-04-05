e59181408ba57b2ef8cac5b8b411ff79e7232cea
Kai Chen <kaichen@outlook.com>
2017-04-05 10:24:07 -0700

    Applying SVM with Gaussian Kernel

--- support-vector-machine/test1.m
+++ support-vector-machine/test1.m
@@ -5,7 +5,6 @@ clear ; close all; clc
 
 fprintf('Loading and Visualizing Data ...\n')
 
-% Load from ex6data1: 
 % You will have X, y in your environment
 load('data1.mat');
 
@@ -17,7 +16,6 @@ pause;
 
 %% ==================== Training Linear SVM ====================
 
-% Load from ex6data1: 
 % You will have X, y in your environment
 load('data1.mat');
 
@@ -43,4 +41,36 @@ fprintf(['Gaussian Kernel between x1 = [1; 2; 1], x2 = [0; 4; -1], sigma = %f :'
          '\n\t%f\n(for sigma = 2, this value should be about 0.324652)\n'], sigma, sim);
 
 fprintf('Press enter to continue.\n');
+pause;
+
+%% =============== Visualizing Dataset 2 ================
+
+fprintf('Loading and Visualizing Data ...\n')
+
+% You will have X, y in your environment
+load('data2.mat');
+
+% Plot training data
+plotData(X, y);
+
+fprintf('Press enter to continue.\n');
+pause;
+
+%% ========== Training SVM with RBF Kernel (Dataset 2) ==========
+
+fprintf('\nTraining SVM with RBF Kernel (this may take 1 to 2 minutes) ...\n');
+
+% You will have X, y in your environment
+load('data2.mat');
+
+% SVM Parameters
+C = 1; sigma = 0.1;
+
+% We set the tolerance and max_passes lower here so that the code will run
+% faster. However, in practice, you will want to run the training to
+% convergence.
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+visualizeBoundary(X, y, model);
+
+fprintf('Program paused. Press enter to continue.\n');
 pause;

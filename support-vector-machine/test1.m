016aedc9a2c478ef4d733b5db74c6cfc70297d6e
Kai Chen <kaichen@outlook.com>
2017-04-05 00:55:39 -0700

    SVM with linear kernel

--- support-vector-machine/test1.m
+++ support-vector-machine/test1.m
@@ -14,3 +14,20 @@ plotData(X, y);
 
 fprintf('Press enter to continue.\n');
 pause;
+
+%% ==================== Training Linear SVM ====================
+
+% Load from ex6data1: 
+% You will have X, y in your environment
+load('data1.mat');
+
+fprintf('\nTraining Linear SVM ...\n')
+
+% You should try to change the C value below and see how the decision
+% boundary varies (e.g., try C = 1000)
+C = 1;
+model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
+visualizeBoundaryLinear(X, y, model);
+
+fprintf('Press enter to continue.\n');
+pause;

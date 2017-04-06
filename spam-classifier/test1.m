6af72486fcadc28a03750d3e3a49252718a73890
Kai Chen <kaichen@outlook.com>
2017-04-06 11:02:16 -0700

    Training SVM with linear kernel for email spam classification

--- spam-classifier/test1.m
+++ spam-classifier/test1.m
@@ -32,3 +32,22 @@ fprintf('Number of non-zero entries: %d\n', sum(features > 0));
 
 fprintf('Press enter to continue.\n');
 pause;
+
+%% =========== Train Linear SVM for Spam Classification ========
+
+% Load the Spam Email dataset
+% You will have X, y in your environment
+load('spamTrain.mat');
+
+fprintf('\nTraining Linear SVM (Spam Classification)\n')
+fprintf('(this may take 1 to 2 minutes) ...\n')
+
+C = 0.1;
+model = svmTrain(X, y, C, @linearKernel);
+
+p = svmPredict(model, X);
+
+fprintf('Training Accuracy: %f\n', mean(double(p == y)) * 100);
+
+fprintf('Press enter to continue.\n');
+pause;

f2c50e3c38d3ff4e30a6615533b67e194ad4ef12
Kai Chen <kaichen@outlook.com>
2017-04-07 19:40:38 -0700

    Computing k-means clustering

--- k-means-clustering/test1.m
+++ k-means-clustering/test1.m
@@ -39,3 +39,28 @@ fprintf('   [ 7.119387 3.616684 ]\n\n');
 
 fprintf('Press enter to continue.\n');
 pause;
+
+%% =================== K-Means Clustering ======================
+%
+fprintf('\nRunning K-Means clustering on example dataset.\n\n');
+
+% Load an example dataset
+load('data.mat');
+
+% Settings for running K-Means
+K = 3;
+max_iters = 10;
+
+% For consistency, here we set centroids to specific values
+% but in practice you want to generate them automatically, such as by
+% settings them to be random examples (as can be seen in
+% kMeansInitCentroids).
+initial_centroids = [3 3; 6 2; 8 5];
+
+% Run K-Means algorithm. The 'true' at the end tells our function to plot
+% the progress of K-Means
+[centroids, idx] = runkMeans(X, initial_centroids, max_iters, true);
+fprintf('\nK-Means Done.\n\n');
+
+fprintf('Press enter to continue.\n');
+pause;

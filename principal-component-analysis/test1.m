9b0b16dc487557b2b131802636ea2792feedc593
Kai Chen <kaichen@outlook.com>
2017-04-09 23:51:58 -0700

    Implementing principal component analysis

--- principal-component-analysis/test1.m
+++ principal-component-analysis/test1.m
@@ -15,3 +15,29 @@ axis([0.5 6.5 2 8]); axis square;
 
 fprintf('Press enter to continue.\n');
 pause;
+
+%% =============== Principal Component Analysis ===============
+
+fprintf('\nRunning PCA on example dataset.\n\n');
+
+%  Before running PCA, it is important to first normalize X
+[X_norm, mu, sigma] = normalizeFeatures(X);
+
+%  Run PCA
+[U, S] = pca(X_norm);
+
+%  Compute mu, the mean of the each feature
+
+%  Draw the eigenvectors centered at mean of data. These lines show the
+%  directions of maximum variations in the dataset.
+hold on;
+drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
+drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
+hold off;
+
+fprintf('Top eigenvector: \n');
+fprintf(' U(:,1) = %f %f \n', U(1,1), U(2,1));
+fprintf('\n(you should expect to see -0.707107 -0.707107)\n');
+
+fprintf('Press enter to continue.\n');
+pause;

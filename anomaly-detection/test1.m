a26e4c5c32a18952ef0f04804bdd4db86fffb183
Kai Chen <kaichen@outlook.com>
2017-04-14 14:55:42 -0700

    Applying anomaly detection to higher dimensional dataset

--- anomaly-detection/test1.m
+++ anomaly-detection/test1.m
@@ -59,3 +59,30 @@ hold off
 
 fprintf('Press enter to continue.\n');
 pause;
+
+%% ================== Multidimensional Outliers ===================
+
+%  Loads the second dataset. You should now have the
+%  variables X, Xval, yval in your environment
+load('data2.mat');
+
+%  Apply the same steps to the larger dataset
+[mu sigma2] = estimateGaussian(X);
+
+%  Training set 
+p = computeGaussian(X, mu, sigma2);
+
+%  Cross-validation set
+pval = computeGaussian(Xval, mu, sigma2);
+
+%  Find the best threshold
+[epsilon F1] = selectThreshold(yval, pval);
+
+fprintf('Best epsilon found using cross-validation: %e\n', epsilon);
+fprintf('Best F1 on Cross Validation Set:  %f\n', F1);
+fprintf('   (you should see a value epsilon of about 1.38e-18)\n');
+fprintf('   (you should see a Best F1 value of 0.615385)\n');
+fprintf('# Outliers found: %d\n\n', sum(p < epsilon));
+
+fprintf('Press enter to continue.\n');
+pause;

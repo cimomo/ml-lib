92dedd95eb1d6596d2fc425b9f8b3450903d0009
Kai Chen <kaichen@outlook.com>
2017-04-15 20:32:18 -0700

    Starting collaborative filtering

---
+++ collaborative-filtering/test1.m
@@ -0,0 +1,27 @@
+%% Initialization
+clear ; close all; clc
+
+%% =============== Loading movie ratings dataset ================
+
+fprintf('Loading movie ratings dataset.\n\n');
+
+%  Load data
+load ('movies.mat');
+
+%  Y is a 1682x943 matrix, containing ratings (1-5) of 1682 movies on 
+%  943 users
+%
+%  R is a 1682x943 matrix, where R(i,j) = 1 if and only if user j gave a
+%  rating to movie i
+
+%  From the matrix, we can compute statistics like average rating.
+fprintf('Average rating for movie 1 (Toy Story): %f / 5\n\n', ...
+        mean(Y(1, R(1, :))));
+
+%  We can "visualize" the ratings matrix by plotting it with imagesc
+imagesc(Y);
+ylabel('Movies');
+xlabel('Users');
+
+fprintf('\nPress enter to continue.\n');
+pause;
